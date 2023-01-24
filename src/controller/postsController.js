ad26f1bcdc59c1d11edd33d799676095051b25f9
Isaac <shocker945516@gmail.com>
2023-01-24 15:29:02 -0800

    crud

--- src/controller/postsController.js
+++ src/controller/postsController.js
@@ -1,19 +1,19 @@
 const Post = require("../model/Post_Model");
 
-exports.getAllPostsPage = (req, res, next) => {
+exports.getAllPostsPage = (req, res) => {
   Post.find()
     .then(([articles]) => {
       console.log(articles);
-      res.render("posts");
+      res.render("posts", { articles: articles });
     })
     .catch((err) => console.error(err.message));
 };
 
-exports.getNewPostPage = (req, res, next) => {
+exports.getNewPostPage = (req, res) => {
   res.render("new_post");
 };
 
-exports.postNewPost = (req, res, next) => {
+exports.postNewPost = (req, res) => {
   const { Title, Content } = req.body;
 
   const newPost = new Post(Title, Content);
@@ -25,9 +25,32 @@ exports.postNewPost = (req, res, next) => {
     .catch((err) => console.error(err.message));
 };
 
-exports.getPostsById = (req, res, next) => {
+exports.getPostsById = (req, res) => {
   const id = req.params.id;
   Post.findById(id)
-    .then(res.render(""))
+    .then(([data]) => {
+      res.render("edit_post", { data: data });
+    })
+    .catch((err) => console.error(err.message));
+};
+
+exports.postEditPostById = (req, res) => {
+  const id = req.params.id;
+  const { Title, Content } = req.body;
+  const data = { Id: id, Title: Title, Content: Content };
+  Post.Update(data)
+    .then(() => {
+      res.redirect("/posts");
+    })
+    .catch((err) => console.error(err.message));
+};
+
+exports.postDeletePost = (req, res) => {
+  const id = req.params.id;
+
+  Post.Delete(id)
+    .then(() => {
+      res.redirect("/posts");
+    })
     .catch((err) => console.error(err.message));
 };
--- src/routes/posts.js
+++ src/routes/posts.js
@@ -3,6 +3,9 @@ const postsController = require("../controller/postsController");
 
 router.get("/", postsController.getAllPostsPage);
 router.get("/new_post", postsController.getNewPostPage);
+router.get("/edit/:id", postsController.getPostsById);
+router.post("/edit/:id", postsController.postEditPostById);
 router.post("/submit", postsController.postNewPost);
+router.delete("/delete/:id", postsController.postDeletePost);
 
 module.exports = router;
